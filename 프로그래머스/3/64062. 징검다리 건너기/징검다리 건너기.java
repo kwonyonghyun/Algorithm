c637d3431baa41accf1b059907a322f6c04353a8
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2025-03-11 10:42:40 +0900

    [level 3] Title: 징검다리 건너기, Time: 21.89 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,35 +1,36 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] stones, int k) {
-        int left = 1;
-        int right = 200000000;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (canCross(stones, k, mid)) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int left=1;
+        int right=200_000_000;// 최대 건널  수 있는 사람들
+        int answer=0;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(canCross(stones, k, mid)){
+                left=mid+1;
+                answer=Math.max(answer, mid);
+            }else{
+                right=mid-1;
             }
         }
-        
-        return right;
+        return answer;
     }
     
-    private boolean canCross(int[] stones, int k, int step) {
-        int zeroCount = 0;
-        
-        for (int stone : stones) {
-            if (stone < step) {
-                zeroCount++;
-                if (zeroCount >= k) return false;
-            } else {
-                zeroCount = 0;
+    public boolean canCross(int[] stones, int k, int mid){
+        int cnt=0;
+        boolean flag=false;
+        for(int i=0;i<stones.length;i++){
+            if(stones[i]< mid){
+                cnt++;
+            }else{
+                cnt=0;
             }
+            if(cnt>=k){
+                flag=true;
+                break;
+            }
+            
         }
-        
-        return true;
+        return !flag;
     }
 }
