e4a5f13277f7703736f27b8b5a504c68c7be61a4
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2025-03-15 19:10:19 +0900

    [Gold V] Title: 택배 배송, Time: 932 ms, Memory: 146572 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -5,21 +5,21 @@ class Pair{
     int cost;
 
     Pair(int to, int cost){
-        this.to = to;
-        this.cost = cost;
+        this.to=to;
+        this.cost =cost;
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
 
-        ArrayList<Pair>[] graph = new ArrayList[n+1];
-        int[] dist = new int[n+1];
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int dist[] = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        for(int i=0;i<n+1;i++){
+        ArrayList<Pair>[] graph = new ArrayList[n+1];
+        for(int i=0;i<=n;i++){
             graph[i]=new ArrayList<>();
         }
 
@@ -27,34 +27,34 @@ public class Main {
             int a=sc.nextInt();
             int b=sc.nextInt();
             int c=sc.nextInt();
+
             graph[a].add(new Pair(b,c));
             graph[b].add(new Pair(a,c));
         }
 
-        dist[1]=0;
         PriorityQueue<Pair> pq = new PriorityQueue<Pair>(
-                (Pair a, Pair b)->{
-                    return a.cost-b.cost;
+                (Pair a, Pair b)-> {
+                    return a.cost - b.cost;
                 }
         );
 
-        pq.add(new Pair(1, 0));
+        pq.add(new Pair(1,0));
+        dist[1]=0;
 
         while(!pq.isEmpty()){
             Pair cur = pq.poll();
 
-            if(dist[cur.to]<cur.cost){
+            if(dist[cur.to] < cur.cost){
                 continue;
             }
 
             for(Pair next: graph[cur.to]){
-                if(dist[next.to]>dist[cur.to]+next.cost){
+                if(dist[next.to]> dist[cur.to]+next.cost){
                     dist[next.to]=dist[cur.to]+next.cost;
-                    pq.add(new Pair(next.to, dist[cur.to]+next.cost));
+                    pq.add(new Pair(next.to, dist[next.to]));
                 }
             }
         }
-        System.out.println(dist[n]);
-
+        System.out.print(dist[n]);
     }
 }
